b91947f8323c45ae6f2f143d5bf6556f5ca839ca
Joshua Byrd <phocks@gmail.com>
2019-09-24 13:20:01 +1000

    Should be working

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 const fs = require("fs");
 const d3 = Object.assign({}, require("d3-dsv"));
+
+// Local modules
 const whichArea = require("../lib/whichArea");
+const timeout = require("../lib/timeout");
+
+// Data
+const classifications = require("../data/electorate-classifications.json");
 
 // Import Australian Electorates
 const electorates = require("../data/federal-electorates-2019.geo.json")
@@ -8,23 +14,89 @@ const electorates = require("../data/federal-electorates-2019.geo.json")
 
 const main = async () => {
   // Read CSV file from disk
-  const importedCSV = fs.readFileSync(
-    "./data/australian_postcodes.csv",
-    "utf8"
-  );
+  const importedCSV = fs.readFileSync("./data/australian_postcodes.csv", {
+    encoding: "utf-8"
+  });
   // Parse file to JSON object
-  const dataList = d3.csvParse(importedCSV);
+  const dataList = d3.csvParseRows(importedCSV, d => {
+    return {
+      postcode: d[0],
+      locality: d[1],
+      state: d[2],
+      long: d[3],
+      lat: d[4],
+      id: d[5],
+      dc: d[6],
+      type: d[7],
+      status: d[8]
+    };
+  });
 
-  let limit = 1;
+  // Clear our output file
+  fs.writeFileSync("output/out.csv", "");
+  fs.appendFileSync(
+    "output/out.csv",
+    "postcode,locality,state,long,lat,id,dc,type,status,electorate,classification\n"
+  );
+
+  // Record limit for testing
+  let limit = 170000;
 
   // Loop through our data
   for (let area of dataList) {
     if (limit === 0) break;
 
-    const result = whichArea([149.13, -35.2809], electorates);
+    const currentLongLat = [area.long, area.lat];
+    // const currentLongLat = [149.13, -35.2809];
 
-    console.log(result.properties);
+    const electorate = whichArea(currentLongLat, electorates)
+      ? whichArea(currentLongLat, electorates).properties.name
+      : "NULL";
 
+    // Cross reference the geo classifications and set if found
+    let classification;
+    if (electorate === "NULL") {
+      classification = "NULL";
+    } else {
+      for (let entry of classifications) {
+        if (entry.division === electorate) {
+          classification = entry.classification;
+          break;
+        }
+      }
+    }
+
+    console.log(area);
+    console.log(electorate);
+    console.log(classification);
+
+    const lineToWrite =
+      area.postcode +
+      "," +
+      area.locality +
+      "," +
+      area.state +
+      "," +
+      area.long +
+      "," +
+      area.lat +
+      "," +
+      area.id +
+      "," +
+      area.dc +
+      "," +
+      area.type +
+      "," +
+      electorate +
+      "," +
+      classification +
+      "\n";
+
+    // Write line
+    fs.appendFileSync("output/out.csv", lineToWrite);
+
+    // Hold up a bit
+    // await timeout(1);
     limit--;
   }
 };
